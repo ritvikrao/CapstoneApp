cf6c56f28789e0a4fed7668afe5b2d83d5abd7f7
Steve Jane <stevejayne1999@gmail.com>
2021-11-14 17:20:49 -0500

    Real-time data and margin change

--- App.js
+++ App.js
@@ -9,40 +9,6 @@ import axios from "axios";
 // username, temp, pH, ammonia, nitrate, nitrite, light_illuminated, last_fed (would represent date)
 // get.body({"id": "615a0f8ce4074662ba8754c5"})
 
-/**
-const DATA = [
-    {
-        id: 'temp',
-        title: 'Water temperature:',
-        data: 0,
-    },
-    {
-        id: 'pH',
-        title: 'Water pH level:',
-        data: 0,
-    },
-    {
-        id: 'ammonia',
-        title: 'Ammonia reading:',
-        data: 0,
-    },
-    {
-        id: 'nitrate',
-        title: 'Nitrate reading:',
-        data: 0,
-    },
-    {
-        id: 'nitrite',
-        title: 'Nitrite reading:',
-        data: 0,
-    },
-    {
-        id: 'light_illuminated',
-        title: 'Illumination:',
-        data: 0,
-    },
-];
- **/
 
 const APP_TITLE = [
     {
@@ -83,16 +49,23 @@ const TitleItem = ({ title }) => (
     </View>
 );
 
+const IDS = ['temp', 'pH', 'ammonia', 'nitrate', 'nitrite', 'light_illuminated'];
+
 export default class App extends React.Component{
     constructor(props) {
         super(props);
-        this.ids = ['temp', 'pH', 'ammonia', 'nitrate', 'nitrite', 'light_illuminated'];
         this.state = {
-            data:[0,0,0,0,0,0] };
+            data:[0,0,0,0,0,0]};
     }
+
     componentDidMount() {
-        for (let i = 0; i < this.ids.length; i++) {
-            fetch("https://smart-aquarium-backend.herokuapp.com/api/" + this.ids[i] + "?id=615a0f8ce4074662ba8754c5", {
+        this.pollAPI();
+        setInterval(() => {this.pollAPI()}, 5000);
+    }
+
+    pollAPI() {
+        for (let i = 0; i < IDS.length; i++) {
+            fetch("https://smart-aquarium-backend.herokuapp.com/api/" + IDS[i] + "?id=615a0f8ce4074662ba8754c5", {
                 method: 'GET',
                 headers: {
                     Accept: 'application/json',
@@ -104,12 +77,10 @@ export default class App extends React.Component{
                     let newResponseData = this.state.data
                     newResponseData[i] = responseData
                     this.setState({data: newResponseData});
-                    console.log(responseData);
                 })
                 .catch(error => console.log(error)) //to catch the errors if any
         }
-        }
-
+    };
 
     render(){
         return (
@@ -125,17 +96,19 @@ export default class App extends React.Component{
                     renderItem={({ item, index, separators}) => (
                         <View style={styles.item}>
                             <Text style={styles.title}>Temperature:</Text>
-                            <Text style={styles.title}>{this.state.data[0]}</Text>
+                            <Text style={styles.value}>{this.state.data[0]}</Text>
                             <Text style={styles.title}>pH:</Text>
-                            <Text style={styles.title}>{this.state.data[1]}</Text>
+                            <Text style={styles.value}>{this.state.data[1]}</Text>
                             <Text style={styles.title}>Ammonia:</Text>
-                            <Text style={styles.title}>{this.state.data[2]}</Text>
+                            <Text style={styles.value}>{this.state.data[2]}</Text>
                             <Text style={styles.title}>Nitrate:</Text>
-                            <Text style={styles.title}>{this.state.data[3]}</Text>
+                            <Text style={styles.value}>{this.state.data[3]}</Text>
                             <Text style={styles.title}>Nitrite:</Text>
-                            <Text style={styles.title}>{this.state.data[4]}</Text>
-                            <Text style={styles.title}>Is the light illuminated?</Text>
-                            <Text style={styles.title}>{this.state.data[5].toString()}</Text>
+                            <Text style={styles.value}>{this.state.data[4]}</Text>
+                            <Text style={styles.title}>Light Status:</Text>
+                            {this.state.data[5] ? (
+                                <Text style={styles.value}>On</Text>) : (
+                                <Text style={styles.value}>Off</Text>)}
                         </View>
                     )}
                 />
@@ -143,88 +116,6 @@ export default class App extends React.Component{
         );
     }
 }
-
-const MyComponent = () => {
-
-    let DATA = [
-        {
-            id: 'temp',
-            title: 'Water temperature:',
-            data: 0,
-        },
-        {
-            id: 'pH',
-            title: 'Water pH level:',
-            data: 0,
-        },
-        {
-            id: 'ammonia',
-            title: 'Ammonia reading:',
-            data: 0,
-        },
-        {
-            id: 'nitrate',
-            title: 'Nitrate reading:',
-            data: 0,
-        },
-        {
-            id: 'nitrite',
-            title: 'Nitrite reading:',
-            data: 0,
-        },
-        {
-            id: 'light_illuminated',
-            title: 'Illumination:',
-            data: 0,
-        },
-    ];
-
-    for (let i = 0; i < DATA.length; i++) {
-        fetch("https://smart-aquarium-backend.herokuapp.com/api/" + DATA[i].id + "?id=615a0f8ce4074662ba8754c5", {
-            method: 'GET',
-            headers: {
-                Accept: 'application/json',
-                'Content-Type': 'application/json',
-            },
-        })
-            .then(response => response.json())
-            .then((responseData) => {
-                DATA[i].data = responseData;
-                console.log(DATA[i].data);
-            })
-            .catch(error => console.log(error)) //to catch the errors if any
-    }
-
-    const renderItem = ({ item }) => (
-        <View style={styles.item}>
-            <Text style={styles.title}>{item.title}</Text>
-            <Text style={styles.title}>{item.data}</Text>
-        </View>
-    );
-
-    const renderTitle = ({ item }) => (
-        <TitleItem title={item.title} />
-    );
-
-    console.log("does get here");
-    console.log(DATA[0].data)
-
-    return (
-        <SafeAreaView style={styles.container}>
-            <FlatList
-                data={APP_TITLE}
-                renderItem={renderTitle}
-                keyExtractor={item => item.id}
-            />
-            <FlatList
-                data={DATA}
-                renderItem={renderItem}
-                keyExtractor={item => item.id}
-            />
-        </SafeAreaView>
-    );
-}
-
 
 const styles = StyleSheet.create({
     container: {
@@ -238,8 +129,12 @@ const styles = StyleSheet.create({
         marginHorizontal: 16,
     },
     title: {
-        fontSize: 24,
+        fontSize: 24
     },
+    value: {
+        fontSize: 24,
+        marginBottom: 15
+    }
 });
 
 const styles2 = StyleSheet.create({
@@ -252,94 +147,9 @@ const styles2 = StyleSheet.create({
         backgroundColor: '#1e5dc2',
         padding: 20,
         marginVertical: 16,
-        marginHorizontal: 16,
+        marginHorizontal: 16
     },
     title: {
-        fontSize: 32,
+        fontSize: 32
     },
 });
-
-/**
-import {
-    StyleSheet,
-    View,
-    ActivityIndicator,
-    FlatList,
-    Text,
-    TouchableOpacity
-} from "react-native";
-export default class App extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            loading: true,
-            dataSource:0 };
-    }
-    componentDidMount(){
-        fetch("https://smart-aquarium-backend.herokuapp.com/api/temp?id=615a0f8ce4074662ba8754c5", {
-            method: 'GET',
-            headers: {
-                Accept: 'application/json',
-                'Content-Type': 'application/json',
-            },})
-            .then(response => response.json())
-            .then((responseData) => {
-                this.loading = false;
-                this.dataSource = responseData;
-            })
-            .catch(error=>console.log(error)) //to catch the errors if any
-    }
-    FlatListItemSeparator = () => {
-        return (
-            <View style={{
-                height: .5,
-                width:"100%",
-                backgroundColor:"rgba(0,0,0,0.5)",
-            }}
-            />
-        );
-    }
-    //<Text style={styles.lightText}>Employee Salary : {data.item.employee_salary}</Text>
-    //<Text style={styles.lightText}>Employee Age : {data.item.employee_age}</Text>
-    renderItem=(data)=>
-        <TouchableOpacity style={styles.list}>
-            <Text style={styles.lightText}>Employee Name : {data}</Text>
-        </TouchableOpacity>
-    render(){
-        if(this.state.loading){
-            console.log("gets here");
-            return(
-                <View style={styles.loader}>
-                    <ActivityIndicator size="large" color="#000000"/>
-                </View>
-            )}
-        console.log(this.state.dataSource)
-        return(
-            <View style={styles.container}>
-                <FlatList
-                    data= {this.state.dataSource}
-                    ItemSeparatorComponent = {this.FlatListItemSeparator}
-                    renderItem= {item=> this.renderItem(item)}
-                    keyExtractor= {item=>item.id.toString()}
-                />
-            </View>
-        )}
-}
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: "#134fd2"
-    },
-    loader:{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-        backgroundColor: "#134fd2"
-    },
-    list:{
-        paddingVertical: 2,
-        margin: 5,
-        backgroundColor: "#fff"
-    }
-});
-**/
